2ff99ce3906d69500588781283a84924e20f0efa
Mohammad Asif Siddique <mdasifsidd@gmail.com>
2022-10-11 18:41:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const btns = document.querySelectorAll(".tab-btn");
+const about = document.querySelector(".about");
+const articles = document.querySelectorAll(".content");
+
+about.addEventListener("click",function(e){
+    // console.log(e.target.dataset.id);
+    const id = e.target.dataset.id;
+    if(id){
+        // remove class from button
+        btns.forEach(function(btn){
+            btn.classList.remove("active");
+            e.target.classList.add("active");
+        });
+        articles.forEach(function(article){
+           article.classList.remove("active"); 
+        })
+        const element = document.getElementById(id);
+        element.classList.add("active");
+        }
+    
+
+})
